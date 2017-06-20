1f5275cfd9bd862692513c688ae14b75b8692a8e
Nathaniel Miller <dukeoflaser@gmail.com>
2017-06-19 18:19:37 -0700

    Data flows through both readable and both writable streams, logging checkpoints, and hits stdout as expected.

--- file_grower.js
+++ file_grower.js
@@ -31,27 +31,77 @@ function maxOf(n) {
   return Math.floor(Math.random() * n);
 }
 
-const rw1 = new stream.Duplex({
-  write(chunk, enc, callback) {
-    rw1.push(chunk.toString().toUpperCase());
-    rw1.push(null);
-  },
-  read(size) {/*...*/},
+
+// *******************************************************
+const readable1 = new stream.Readable({
+  read(size){/*...*/}
 });
 
-const rw2 = new stream.Duplex({
-  write(chunk, enc, callback) {
-    rw2.push(chunk.toString().split(',').join(''));
-    rw2.push(null);
-  },
-  read(size) {/*...*/},
+const readable2 = new stream.Readable({
+  read(size){/*...*/}
 });
+
+const writable1 = new stream.Writable({
+  write(data, enc, callback){
+    console.log('CheckPoint 2', data);
+    readable1.push(data);
+    callback();
+  }
+});
+
+const writable2 = new stream.Writable({
+  write(data, enc, callback){
+    console.log('CheckPoint 4', data);
+    readable2.push(data);
+    callback();
+  }
+});
+
+// -----  -----  -------   ----- ----   ---------
+//Readable Stream gets data -> writes that data to a writeable stream.
+//That writable stream pushes the data to the next readable stream.
+
+process.stdin.on('data', data => {
+  console.log('CheckPoint 1', data);
+  writable1.write(data, () => {
+    console.log('CheckPoint 2.b', data);
+  });
+});
+
+// readable1.pipe(writeable2);
+readable1.on('data', data => {
+  console.log('CheckPoint 3', data);
+  writable2.write(data, () => {
+    console.log('CheckPoint 4.b');
+  });
+});
+
+readable2.pipe(process.stdout)
+readable2.on('end', () => {console.log('Its Over')})
+// *******************************************************
+
+
+// const rw1 = new stream.Duplex({
+//   write(chunk, enc, callback) {
+//     rw1.push(chunk.toString().toUpperCase());
+//     rw1.push(null);
+//   },
+//   read(size) {/*...*/},
+// });
+//
+// const rw2 = new stream.Duplex({
+//   write(chunk, enc, callback) {
+//     rw2.push(chunk.toString().split(',').join(''));
+//     rw2.push(null);
+//   },
+//   read(size) {/*...*/},
+// });
 
 setInterval(growFile, 5000);
 
-process.stdin.pipe(rw1);
-rw1.pipe(rw2);
-rw2.pipe(process.stdout);
+// process.stdin.pipe(rw1);
+// rw1.pipe(rw2);
+// rw2.pipe(process.stdout);
 
 // process.stdin.pipe(process.stdout); //Works as expected.
 
